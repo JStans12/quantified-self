bf58fd5e38792f89ca6d4a868d2240daa6ed7bf7
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-01-25 15:43:57 -0700

    adding foods tested

--- test/foods_html_test.js
+++ test/foods_html_test.js
@@ -4,7 +4,7 @@ var test = require('selenium-webdriver/testing');
 
 test.describe('testing foods.html', function(){
   var driver;
-  this.timeout(10000);
+  this.timeout(20000);
 
   test.beforeEach(function(){
     driver = new webdriver.Builder()
@@ -31,6 +31,77 @@ test.describe('testing foods.html', function(){
     calories.sendKeys(500);
     calories.getAttribute('value').then(function(value){
       assert.equal(value, '500');
+    });
+  });
+
+  test.it("allows me add a new food", function(){
+
+    driver.get('http://localhost:8080/foods.html')
+
+    var name = driver.findElement({name: 'name'});
+    var calories = driver.findElement({name: 'calories'});
+    var addFoodButton = driver.findElement({id: 'add-food-button'});
+
+    name.sendKeys('orange');
+    calories.sendKeys(500);
+    addFoodButton.click();
+    driver.sleep(1000);
+
+    driver.findElement({id: 'foods-table'}).then(function(table){
+      table.findElements(webdriver.By.css('tr')).then(function(rows){
+        assert.equal(rows.length, 2);
+
+        rows[1].findElement(webdriver.By.className('food-name-cell')).getText().then(function(foodName){
+          assert.equal(foodName, 'orange');
+        });
+      });
+    });
+  });
+
+  test.it("doesn't allow me to add foods if name is empty", function(){
+
+    driver.get('http://localhost:8080/foods.html')
+
+    var name = driver.findElement({name: 'name'});
+    var calories = driver.findElement({name: 'calories'});
+    var addFoodButton = driver.findElement({id: 'add-food-button'});
+
+    name.sendKeys('');
+    calories.sendKeys(500);
+    addFoodButton.click();
+    driver.sleep(500);
+
+    driver.findElement({id: 'foods-table'}).then(function(table){
+      table.findElements(webdriver.By.css('tr')).then(function(rows){
+        assert.equal(rows.length, 1);
+      });
+    });
+
+    driver.findElement({id: 'name-error'}).getText().then(function(errorText){
+      assert.equal(errorText, 'Please enter a food name.')
+    });
+  });
+
+  test.it("doesn't allow me to add foods if calories is empty", function(){
+
+    driver.get('http://localhost:8080/foods.html')
+
+    var name = driver.findElement({name: 'name'});
+    var calories = driver.findElement({name: 'calories'});
+    var addFoodButton = driver.findElement({id: 'add-food-button'});
+
+    name.sendKeys('orange');
+    addFoodButton.click();
+    driver.sleep(500);
+
+    driver.findElement({id: 'foods-table'}).then(function(table){
+      table.findElements(webdriver.By.css('tr')).then(function(rows){
+        assert.equal(rows.length, 1);
+      });
+    });
+
+    driver.findElement({id: 'calorie-error'}).getText().then(function(errorText){
+      assert.equal(errorText, 'Please enter a calorie count.')
     });
   });
 });
