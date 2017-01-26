856592755677ae10f6c3abe6b94939d66c8ada25
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-01-26 09:39:07 -0700

    save when you click outside the cell

---
+++ test/user_can_update_food_test.js
@@ -0,0 +1,42 @@
+var assert = require('chai').assert;
+var webdriver = require('selenium-webdriver');
+var test = require('selenium-webdriver/testing');
+
+test.describe('updating foods', function(){
+  var driver;
+  this.timeout(20000);
+
+  test.beforeEach(function(){
+    driver = new webdriver.Builder()
+      .forBrowser('chrome')
+      .build();
+  });
+
+  test.afterEach(function(){
+      driver.quit();
+  });
+
+  test.it("should allow me to update food names", function(){
+
+    driver.get('http://localhost:8080/foods.html')
+
+    var name = driver.findElement({name: 'name'});
+    var calories = driver.findElement({name: 'calories'});
+    var addFoodButton = driver.findElement({id: 'add-food-button'});
+
+    name.sendKeys('orange');
+    calories.sendKeys(500);
+    addFoodButton.click();
+    driver.sleep(1000);
+
+    driver.findElement({id: 'foods-table'}).then(function(table){
+      table.findElements(webdriver.By.css('tr')).then(function(rows){
+        rows[1].findElement(webdriver.By.className('food-name-cell')).then(function(foodNameCell){
+          foodNameCell.click()
+        });
+      });
+    });
+
+    driver.sleep(1000);
+  });
+});
