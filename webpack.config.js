04a55b2b0d977939a34a8ccd99752217d5618eff
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-01-28 09:25:37 -0700

    webpack fixed for exercises

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    main: "./lib/index.js",
+    exercises: "./lib/exercises_index.js",
     foods: "./lib/foods_index.js",
     test: "mocha!./test/index.js"
   },
